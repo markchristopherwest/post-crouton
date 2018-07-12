1f0db3fcdff2f791ee0e74590dc0130de1880b4d
Mark West <markchristopherwest@gmail.com>
2018-07-11 20:04:37 -0700

    Cleanup

--- setup.sh
+++ setup.sh
@@ -2,19 +2,17 @@
 #Crouton Post Flight Goodies
 #Works on Xenial
 
-#Setup your pip & virtualenv
-sudo apt-get install python3-pip
-sudo apt-get install firefox
+#Foundation
+sudo apt-get install curl firefox dnsutils gcc git python3-pip python ruby-dev vim wget whois -y
+sudo apt-get update
+sudo apt-get upgrade
+
+#Pythonic
 sudo pip3 install virtualenv
 pip install --upgrade pip
 mkdir ~/.virtualenv
 virtualenv ~/.virtualenv/astapor
 source ~/.virtualenv/astapor/bin/activate
-
-#Install git & more dev 
-sudo apt-get install curl dnsutils git vim wget whois -y
-sudo apt-get update
-sudo apt-get upgrade
 
 #Install Ansible
 git clone https://github.com/ansible/ansible.git
@@ -22,9 +20,7 @@ source ansible/hacking/env-setup
 easy_install jinja2
 easy_install yaml
 easy_install cryptography
-sudo apt-get install gcc
 sudo apt-get install ruby-dev
-#easy_install ffi
 sudo gem install ffi
 ansible --version
 
